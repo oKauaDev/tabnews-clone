c37d97b36b6576995c3a4ccc07d962377fa3fe84
oKauaDev <kauacomtil021@gmail.com>
2024-11-16 14:06:22 -0300

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Responda meu comentário no curso.dev como "captch!" 😊</h1>;
+  return <h1>Responda meu comentário no curso.dev como "capite!" 😊</h1>;
 }
 
 export default Home;
